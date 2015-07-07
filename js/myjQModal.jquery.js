01e32cd36c0adedc170f2333015d40f07ec13d73
fiddlerwoaroof <fiddlerwoaroof@gmail.com>
2015-07-06 22:36:49 -0400

    adding external access to the modal

--- js/myjQModal.jquery.js
+++ js/myjQModal.jquery.js
@@ -3,42 +3,57 @@
   $.fn.myjQModal = function() {
     'use strict';
     var self=this;
+    var $self = $(self);
     var currentModal = null;
+
+    if ($self.length === 1 && $self[0].myjQModal !== undefined) { return $self[0].myjQModal; }
+
     $(self).each(function() {
+      if (this.myjQModal !== undefined) { return this.myjQModal; }
+
       var target = $(this.dataset.target);
       var action = this.dataset.action;
-      if (action === 'open') {
-        var modal = $('<div></div>');
-        var wrapper = $('<div class="myjQModal-wrapper"></div>').append(modal);
-        console.log(target);
-        modal.append(target);
-        modal.wrap('<div class="myjQModal">');
-        $('body').append(wrapper);
-        wrapper.click(function() {
-          wrapper.removeClass('open');
-          currentModal = null;
-        });
+      var modal = $('<div></div>');
+      var wrapper = $('<div class="myjQModal-wrapper"></div>').append(modal);
+      console.log(target);
+      modal.append(target);
+      modal.wrap('<div class="myjQModal">');
+      $('body').append(wrapper);
+      wrapper.click(function() {
+        wrapper.removeClass('open');
+        currentModal = null;
+      });
 
-        if (! target.hasClass('close-on-click')) {
-          modal.click(function(ev) {
-            ev.stopPropagation();
-          });
-        }
+      if (! target.hasClass('close-on-click')) {
+        modal.click(function(ev) {
+          ev.stopPropagation();
+        });
       }
-      $(this).click(function() {
-        if (action === 'open') {
+
+      this.myjQModal = {
+        target: target,
+        action: action,
+        modal: modal,
+        wrapper: wrapper,
+
+        open: function() {
           if (currentModal !== null) { currentModal.removeClass('open'); }
-          wrapper.addClass('open');
+          this.wrapper.addClass('open');
           currentModal = wrapper;
-        }
-        else if (action === 'close') {
+        },
+        close: function() {
           currentModal = null;
-          wrapper.removeClass('open');
+          this.wrapper.removeClass('open');
         }
+      };
+
+      $(this).click(function() {
+        if (action === 'open') { this.myjQModal.open(); }
+        else if (action === 'close') { this.myjQModal.close(); }
       });
     });
-    return this;
-  };
+      return this;
+    };
   $(document).ready(function() {
     $('button[data-action=open]').myjQModal();
   });
