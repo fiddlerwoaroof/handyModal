2ebc712ba486a352630b4f3ee5a6187595030ae1
Ed Langley <fiddler.wo.a.roof@gmail.com>
2016-03-18 16:58:19 +0000

    Handle edge-cases better, don't auto-init

--- js/myjQModal.jquery.js
+++ js/myjQModal.jquery.js
@@ -2,61 +2,68 @@
 
   var currentModal = null;
   $.fn.myjQModal = function() {
-    'use strict';
-    var self=this;
-    var $self = $(self);
+      'use strict';
+      var self=this;
+      var $self = $(self);
 
-    if ($self.length === 1 && $self[0].myjQModal !== undefined) { return $self[0].myjQModal; }
+      if ($self.length === 1 && $self[0].myjQModal !== undefined) { return $self[0].myjQModal; }
 
-    $(self).each(function() {
-      if (this.myjQModal !== undefined) { return this.myjQModal; }
+      $(self).each(function() {
+          if (this.myjQModal === undefined) {
+              var target = $(this.dataset.target);
+              var targetModal = target.data('myjQModal');
+              var action = this.dataset.action;
+              var wrapper = target.closest('.myjQModal-wrapper');
 
-      var target = $(this.dataset.target);
-      var action = this.dataset.action;
-      var modal = $('<div></div>');
-      var wrapper = $('<div class="myjQModal-wrapper"></div>').append(modal);
-      //console.log(target);
-      modal.append(target);
-      modal.wrap('<div class="myjQModal">');
-      $('body').append(wrapper);
-      wrapper.click(function() {
-        wrapper.removeClass('open');
-        currentModal = null;
+              if (targetModal === undefined) {
+                  var modal = $('<div></div>');
+                  wrapper = $('<div class="myjQModal-wrapper"></div>').append(modal);
+
+                  modal.append(target);
+                  modal.wrap('<div class="myjQModal">');
+                  $('body').append(wrapper);
+
+                  wrapper.click(function() {
+                      wrapper.removeClass('open');
+                      currentModal = null;
+                  });
+
+                  if (! target.hasClass('close-on-click')) {
+                      modal.click(function(ev) {
+                          ev.stopPropagation();
+                      });
+                  }
+
+                  target.removeClass('preinit');
+              }
+
+              this.myjQModal = {
+                  target: target,
+                  action: action,
+                  wrapper: wrapper,
+
+                  open: function() {
+                      this.wrapper.addClass('open');
+                      if (currentModal !== null) { currentModal.removeClass('open'); }
+                      currentModal = wrapper;
+                  },
+                  close: function() {
+                      currentModal = null;
+                      this.wrapper.removeClass('open');
+                  }
+              };
+
+              if (targetModal === undefined) {
+                  target.data('myjQModal', this.myjQModal);
+              }
+
+              $(this).click(function() {
+                  console.log('this should happen');
+                  if (action === 'open') { this.myjQModal.open(); }
+                  else if (action === 'close') { this.myjQModal.close(); }
+              });
+          };
       });
-
-      if (! target.hasClass('close-on-click')) {
-        modal.click(function(ev) {
-          ev.stopPropagation();
-        });
-      }
-
-      target.myjQModal = this.myjQModal = {
-        target: target,
-        action: action,
-        modal: modal,
-        wrapper: wrapper,
-
-        open: function() {
-          this.wrapper.addClass('open');
-          if (currentModal !== null) { currentModal.removeClass('open'); }
-          currentModal = wrapper;
-        },
-        close: function() {
-          currentModal = null;
-          this.wrapper.removeClass('open');
-        }
-      };
-
-      $(this).click(function() {
-          console.log('this should happen');
-        if (action === 'open') { this.myjQModal.open(); }
-        else if (action === 'close') { this.myjQModal.close(); }
-      });
-    });
       return this;
-    };
-  $(document).ready(function() {
-      console.log('boo');
-    $('button[data-type=modalTrigger]').myjQModal();
-  });
+  };
 }( jQuery ));
